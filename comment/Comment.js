3f65f6675db5005dc9c894f8dbbb89bdf8a703dd
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-09 18:04:10 -0300

    Add Comment model

---
+++ comment/Comment.js
@@ -0,0 +1,13 @@
+// Comment.js
+
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  start: Number,
+  end: Number,
+  text: String
+});
+
+mongoose.model("Comment", CommentSchema);
+
+module.exports = mongoose.model("User");
