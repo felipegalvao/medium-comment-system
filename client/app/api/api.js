bb90f4d61aaefc4b9d85850d94b6c3440a298338
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-10 14:05:41 -0300

    Create API and method to get comments from DB

---
+++ client/app/api/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+module.exports = {
+  getComments: () => {
+    return axios
+      .get("http://localhost:3000/comments")
+      .then(res => {
+        return res.data;
+      })
+      .catch(res => {
+        return "An error occurred";
+      });
+  }
+};
