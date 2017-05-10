07039a01506a661f64fe12da5c4a5e6dffb7c709
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-09 22:32:06 -0300

    Add home page route

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const db = require("./db");
 const CommentController = require("./comment/CommentController");
 app.use("/comments", CommentController);
 
+app.get('/', function (req, res) {
+  res.send('Home Page');
+});
+
 module.exports = app;
