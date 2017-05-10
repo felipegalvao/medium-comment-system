01389b3a84d4828fc03071a39d40c796641faf3e
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-10 20:12:30 -0300

    Add button to show form

--- client/app/components/AddComment.js
+++ client/app/components/AddComment.js
@@ -1,17 +1,40 @@
 import React, { Component } from "react";
 
 class AddComment extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showForm: false
+    };
+  }
+
+  renderForm = () => {
+    if (this.state.showForm) {
+      return (
+        <form>
+          <div className="form-group">
+            <label htmlFor="comment" className="text-center">
+              Add your comment below
+            </label>
+            <textarea className="form-control" id="comment" rows="4" />
+          </div>
+          <button className="btn btn-primary btn-block">Add Comment</button>
+        </form>
+      );
+    }
+  };
+
   render() {
     return (
       <div className="row">
         <div className="col-xs-6 col-xs-offset-3">
-          <form>
-            <div className="form-group">
-              <label htmlFor="comment" className="text-center">Add your comment below</label>
-              <textarea className="form-control" id="comment" rows="4" />
-            </div>
-            <button className="btn btn-primary btn-block">Add Comment</button>
-          </form>
+          <button
+            className="btn btn-block"
+            onClick={() => this.setState({ showForm: true })}
+          >
+            Add Comment
+          </button>
+          {this.renderForm()}
         </div>
       </div>
     );
