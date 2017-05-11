fed2806f5abd367eb30dbfeb135d2fc8883346de
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-10 21:40:17 -0300

    AddComment button now gets the selected text

--- client/app/components/AddComment.js
+++ client/app/components/AddComment.js
@@ -4,21 +4,33 @@ class AddComment extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showForm: false
+      showForm: false,
+      selectedText: ""
     };
   }
+
+  onShowFormClick = () => {
+    let selectedText;
+    if (window.getSelection) {
+      selectedText = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+      selectedText = document.selection.createRange().text;
+    }
+    this.setState({ showForm: true, selectedText: selectedText });
+  };
 
   renderForm = () => {
     if (this.state.showForm) {
       return (
         <form>
+          {this.state.selectedText === "" ? "" : <p>Comment for: {this.state.selectedText}</p>}
           <div className="form-group">
             <label htmlFor="comment" className="text-center">
               Add your comment below
             </label>
             <textarea className="form-control" id="comment" rows="4" />
           </div>
-          <button className="btn btn-primary btn-block">Add Comment</button>
+          <button className="btn btn-primary btn-block">Finish Adding Comment</button>
         </form>
       );
     }
@@ -28,10 +40,7 @@ class AddComment extends Component {
     return (
       <div className="row">
         <div className="col-xs-6 col-xs-offset-3">
-          <button
-            className="btn btn-block"
-            onClick={() => this.setState({ showForm: true })}
-          >
+          <button className="btn btn-block" onClick={() => this.onShowFormClick()}>
             Add Comment
           </button>
           {this.renderForm()}
