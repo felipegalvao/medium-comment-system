49051df7d51c6b8f1c54d6509044bf149aca2a04
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-10 23:41:12 -0300

    Remove mount from App component test

--- client/__tests__/components/App.test.js
+++ client/__tests__/components/App.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {expect} from "chai";
-import { shallow, mount } from "enzyme";
+import { shallow } from "enzyme";
 import App from "../../app/components/App";
 
 describe("App", () => {
