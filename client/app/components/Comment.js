7148931e00b7b7804dd3ac282685dc9921f62fd5
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-10 23:38:47 -0300

    Styles for the Comment component

--- client/app/components/Comment.js
+++ client/app/components/Comment.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Comment = ({ text, start, end, postText }) => {
+const Comment = ({ text, postFragment, postText }) => {
   return (
-    <div>
-      <p>Comment related to: {postText.slice(start, end+1)}</p>
-      <p>{text} (starts at {start} and ends at {end})</p>
+    <div className="comment">
+      {postFragment === "" ? "" : <p className="comment__post-fragment">{postFragment}</p>}
+      <p>{text}</p>
     </div>
   );
 };
