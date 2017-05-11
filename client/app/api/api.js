a93344a9cff9e3dd5a3973bd81c0754685943388
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-10 22:55:18 -0300

    Fix for saveComment

--- client/app/api/api.js
+++ client/app/api/api.js
@@ -15,7 +15,7 @@ module.exports = {
       });
   },
 
-  saveComment: (postFragment, text) => {        
+  saveComment: (postFragment, text) => {
     return axios({
       method: "post",
       url: "http://localhost:3000/comments",
@@ -25,7 +25,7 @@ module.exports = {
       }
     })
       .then(res => {
-        console.log(res);
+        return res;
       })
       .catch(error => {
         console.log(error);
