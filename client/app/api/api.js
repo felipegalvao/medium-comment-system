9361dd160ca7cf729a76080e4160291e83445db1
Felipe Galvao <contato@felipegalvao.com.br>
2017-05-10 22:02:37 -0300

    Add method to save comment

--- client/app/api/api.js
+++ client/app/api/api.js
@@ -8,11 +8,21 @@ module.exports = {
         return res.data;
       })
       .catch(res => {
-        return "An error has occured"
+        return "An error has occured";
       });
   },
 
-  consoleTest: () => {
-    console.log("consoleTest");
+  saveComment: (postFragment, text) => {
+    return axios
+      .post("http://localhost:3000/comments", {
+        postFragment: postFragment,
+        text: text
+      })
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 };
